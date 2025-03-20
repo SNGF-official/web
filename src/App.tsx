b55bc50a09bce2c8f30961b07e2d39931cb489f2
YlanDecode <rabenaly3@gmail.com>
2025-03-20 20:12:07 +0300

    chore: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import './App.css';
+import './App.css'
 
-import { lazy, Suspense } from 'react';
-import { Loader } from '@/components/loading/loading.tsx';
+import { Suspense } from "react";
+import {Loader} from "@/components/loading/loading.tsx";
 
-const LandingPage = lazy(() =>
-  import('@/page/landing/Landing').then((module) => ({
-    default: module.Landing,
-  }))
-);
+// const ProductList = lazy(() => import("./components/ProductList"));
 
 export default function App() {
-  return (
-    <Suspense fallback={<Loader />}>
-      <LandingPage />
-    </Suspense>
-  );
+    return (
+        <Suspense fallback={<Loader/>}>
+            {/**<ProductList />*/}
+            okokokokoko
+        </Suspense>
+    );
 }
--- src/config/env.ts
+++ src/config/env.ts
@@ -1,10 +1,10 @@
 declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      TINY_MCE_API_KEY: string;
-      BASE_URL: string;
+    namespace NodeJS {
+        interface ProcessEnv {
+            TINY_MCE_API_KEY: string;
+            BASE_URL: string;
+        }
     }
-  }
 }
 
 export default process.env;
